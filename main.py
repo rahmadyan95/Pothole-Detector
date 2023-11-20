7d02d79d037f79ce66159f06f5c92ea33c622766
Rahmadyan <haekal.rahmadyan95@gmail.com>
2023-11-20 08:22:00 +0700

    add: photo and button

--- main.py
+++ main.py
@@ -60,45 +60,55 @@ class StartPage(ctk.CTkFrame):
                           font=("Bahnschrift SemiBold SemiConden",45),
                           hover_color="#fce101",image=kamera_image,
                           
-                          
                           )
         page1_button.place(x=100,y=200)
 
+        
+        image_path1 = os.path.join(os.path.dirname(__file__), 'app_asset\maps.png')
+        image1 = CTkImage(light_image=Image.open(image_path1), size=(90,90))
         page2 = CTkButton(self, width=240, 
-                          height=300, text='SHOW DATA',
-                          fg_color='grey', text_color='black',font=("Bahnschrift SemiBold SemiConden",18),
+                          height=300, text='\nSHOW MAPS',
+                          fg_color='grey', text_color='black',font=("Bahnschrift SemiBold SemiConden",22),
                           command=lambda: controller.show_frame(Page2),
-                          textvariable=(20,10),
                           hover_color="#fce101",
-                          
+                          image=image1
+
                           )
         page2.place(x=100,y=370)
 
-        page2 = CTkButton(self, width=240, 
+        
+        # image_label1 = CTkLabel(page2,image=image1,text='',bg_color='grey20')
+        # image_label1.place(x=1000)
+
+        page3 = CTkButton(self, width=240, 
                           height=300, text='SHOW DATA',
                           fg_color='grey', text_color='black',
                           command=lambda: controller.show_frame(Page3),
                           hover_color="#fce101",
                           
                           )
-        page2.place(x=360,y=370)
+        page3.place(x=360,y=370)
 
 
         side_background = Canvas(self, width=1180, height= 940, background="grey", highlightthickness=0)
         side_background.place(x=1300,y=400)
 
-        title_side = CTkLabel(self,text="SYSTEM INFORMATION", font=("Bahnschrift SemiBold SemiConden",18),text_color="white",bg_color="grey")
+        title_side = CTkLabel(self,text="SYSTEM INFORMATION", font=("Bahnschrift SemiBold SemiConden",18),text_color="grey20",bg_color="grey")
         title_side.place(x=872, y=220)
 
 
+        #============================= Untuk memasukan Foto ==================================================== #
+        image_path = os.path.join(os.path.dirname(__file__), 'app_asset\construction.png')
+        image = CTkImage(light_image=Image.open(image_path), size=(90,90))
+        image_label = CTkLabel(self,image=image,text='',bg_color='grey20')
+        image_label.place(x=375,y=20)
+        #============================= Untuk memasukan Foto ==================================================== #
+
+        
     
 
-        
 
-        
-
-
-
+    
 
         # Button untuk mengarahkan ke Page2
         # page2_button = CTkButton(self, width=220,
